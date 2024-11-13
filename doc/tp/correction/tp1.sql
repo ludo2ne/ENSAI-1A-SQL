453f461ef44177abbfe6ceb1e5f1c84975944187
Ludovic Deneuville <ludovic.deneuville@ensai.fr>
2024-11-13 16:59:44 +0100

    tp1 et 2 almost done

--- doc/tp/correction/tp1.sql
+++ doc/tp/correction/tp1.sql
@@ -45,6 +45,77 @@ SELECT *
  ORDER BY sexe,
           nombre DESC;
 
+SELECT *
+  FROM prenom
+ WHERE preusuel <> '_PRENOMS_RARES'
+   AND annais <> 'XXXX'
+   AND annais = '2022'
+   AND sexe = 2
+   AND preusuel LIKE 'Q%';
+
+SELECT SUBSTRING(preusuel, 1, 1) AS lettre,
+       SUM(nombre)
+  FROM prenom
+ WHERE preusuel <> '_PRENOMS_RARES'
+   AND annais <> 'XXXX'
+   AND annais = '2022'
+ GROUP BY SUBSTRING(preusuel, 1, 1)
+ ORDER BY 1;
+
+SELECT SUBSTRING(preusuel, 1, 1) AS lettre, --preusuel[:1],
+       SUM(nombre) FILTER (WHERE sexe = 1) AS garcon,
+       SUM(nombre) FILTER (WHERE sexe = 2) AS fille
+  FROM prenom
+ WHERE preusuel <> '_PRENOMS_RARES'
+   AND annais <> 'XXXX'
+   AND annais = '2022'
+ GROUP BY SUBSTRING(preusuel, 1, 1)
+ ORDER BY 1;
+
+
+-------------------------------------------------------------------------------
+-- Statistiques descriptives
+-------------------------------------------------------------------------------
+
+SELECT preusuel,
+       LENGTH(preusuel)
+  FROM prenom
+ WHERE preusuel <> '_PRENOMS_RARES'
+   AND annais = '2003';
+
+SELECT MIN(LENGTH(preusuel)),
+       MAX(LENGTH(preusuel)),
+       AVG(LENGTH(preusuel))
+  FROM prenom
+ WHERE preusuel <> '_PRENOMS_RARES'
+   AND annais = '2003';
+
+SELECT preusuel
+  FROM prenom
+ WHERE annais = '2003'
+   AND LENGTH(preusuel) = (SELECT MAX(LENGTH(preusuel)) 
+                             FROM prenom 
+                            WHERE annais = '2003');
+
+SELECT annais,
+       preusuel,
+       LENGTH(preusuel)
+  FROM prenom p1
+ WHERE preusuel <> '_PRENOMS_RARES'
+   AND annais BETWEEN '2015' AND '2022'
+   AND LENGTH(preusuel) = (SELECT MAX(LENGTH(preusuel)) 
+                             FROM prenom p2
+                            WHERE preusuel <> '_PRENOMS_RARES'
+                              AND p1.annais = p2.annais)
+ ORDER BY annais;
+
+SELECT annais,
+       ROUND(AVG(LENGTH(preusuel)),1)
+  FROM prenom
+ WHERE preusuel <> '_PRENOMS_RARES'
+   AND annais <> 'XXXX'
+ GROUP BY annais
+ ORDER BY annais;
 
 -------------------------------------------------------------------------------
 -- Jérôme
@@ -138,7 +209,7 @@ SELECT preusuel,
 
 
 -------------------------------------------------------------------------------
--- 2016
+-- Cette année-là
 -------------------------------------------------------------------------------
 
 -- Nombre de prénoms distincts en 2016
@@ -148,9 +219,9 @@ SELECT COUNT(distinct preusuel) AS nb_prenoms,
        prenoms_filles + prenoms_garcons
   FROM prenom
  WHERE preusuel <> '_PRENOMS_RARES'
-   AND annais = '2016';
+   AND annais = '1962';
 
--- Prénoms 2016 donnés à des filles et des garçons
+-- Prénoms 1962 donnés à des filles et des garçons
 SELECT preusuel,
        SUM(nombre) AS total,
        SUM(nombre) FILTER (WHERE sexe = 2) AS total_filles,
@@ -158,7 +229,7 @@ SELECT preusuel,
   FROM prenom
  WHERE preusuel <> '_PRENOMS_RARES'
    AND annais <> 'XXXX'
-   AND annais = '2016'
+   AND annais = '1962'
  GROUP BY preusuel
  HAVING COUNT(DISTINCT sexe) = 2  -- Le prénom doit avoir été donné aux deux sexes
 ORDER BY total DESC;
@@ -172,8 +243,51 @@ SELECT preusuel,
   FROM prenom
  WHERE preusuel <> '_PRENOMS_RARES'
    AND annais <> 'XXXX'
-   AND annais = '2016'
+   AND annais = '1962'
  GROUP BY preusuel
  HAVING COUNT(DISTINCT sexe) = 2  -- Le prénom doit avoir été donné aux deux sexes
     AND total_filles > total_garcons  -- Plus donné aux filles
 ORDER BY total DESC;
+
+
+
+-------------------------------------------------------------------------------
+-- Fichier des individus
+-------------------------------------------------------------------------------
+
+CREATE OR REPLACE VIEW individus AS
+FROM 'https://static.data.gouv.fr/resources/recensement-de-la-population-fichiers-detail-individus-localises-au-canton-ou-ville-2020-1/20231023-122841/fd-indcvi-2020.parquet'; 
+
+SELECT COUNT(1)
+  FROM individus;
+
+SELECT SUM(ipondi)::INT
+  FROM individus;
+
+SELECT *
+  FROM individus
+ LIMIT 10;
+
+CREATE OR REPLACE VIEW variables_individus AS
+FROM 'https://static.data.gouv.fr/resources/recensement-de-la-population-fichiers-detail-individus-localises-au-canton-ou-ville-2020-1/20231025-082910/dictionnaire-variables-indcvi-2020.csv'
+
+SELECT *
+  FROM variables_individus;
+
+SELECT *
+  FROM variables_individus
+ WHERE lib_var ILIKE '%partem%';
+
+SELECT dept,
+       SUM(ipondi)::INT AS nb_hab
+  FROM individus
+ GROUP BY dept
+ ORDER BY 1;
+
+SELECT dept,
+       SUM(ipondi) FILTER(WHERE sexe = 1)::INT AS hommes,
+       SUM(ipondi) FILTER(WHERE sexe = 2)::INT AS femmes
+  FROM individus
+ WHERE ager20 = 29
+ GROUP BY dept
+ ORDER BY 1;
