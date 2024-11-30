f0ae01de09c0e595fc3132a2a3003a24e8761492
Ludovic Deneuville <ludovic.deneuville@ensai.fr>
2024-11-30 20:00:50 +0100

    TP4 almost done

--- doc/tp/correction/tp4.sql
+++ doc/tp/correction/tp4.sql
@@ -145,17 +145,48 @@ HAVING COUNT(1) > 5;
 -- Afficher les résultat de parties
 ----------------------------------------------------------------------
 
-SELECT CONCAT_WS(' ', LOWER(j1.code_titre), j1.nom, j1.prenom) AS blancs,
+SELECT j1.titre,
+       j1.nom
+       j1.prenom,
        j1.elo,
        rp.resultat,
-       j2.elo,
-       CONCAT_WS(' ', LOWER(j2.code_titre), j2.nom, j2.prenom) AS noirs
+       j2.titre,
+       j2.nom,
+       j2.prenom,
+       j2.elo
   FROM echecs.joueuse j1
   JOIN echecs.partie p ON (p.id_blanc = j1.id_joueuse)
   JOIN echecs.joueuse j2 ON (p.id_noir = j2.id_joueuse)
   JOIN echecs.resultat_partie rp USING(id_resultat);
 
--- Victoires surprises
+
+-- Affichage résultats parties joli
+SELECT CONCAT_WS(' ', LOWER(j1.code_titre), j1.nom, j1.prenom) AS blancs,
+       j1.elo,
+       rp.resultat AS res,
+       CONCAT_WS(' ', LOWER(j2.code_titre), j2.nom, j2.prenom) AS noirs,
+       j2.elo
+  FROM echecs.joueuse j1
+  JOIN echecs.partie p ON (p.id_blanc = j1.id_joueuse)
+  JOIN echecs.joueuse j2 ON (p.id_noir = j2.id_joueuse)
+  JOIN echecs.resultat_partie rp USING(id_resultat);
+
+
+-- Blitz à Rennes
+SELECT CONCAT_WS(' ', LOWER(j1.code_titre), j1.nom, j1.prenom) AS blancs,
+       j1.elo,
+       rp.resultat AS res,
+       CONCAT_WS(' ', LOWER(j2.code_titre), j2.nom, j2.prenom) AS noirs,
+       j2.elo
+  FROM echecs.joueuse j1
+  JOIN echecs.partie p ON (p.id_blanc = j1.id_joueuse)
+  JOIN echecs.joueuse j2 ON (p.id_noir = j2.id_joueuse)
+  JOIN echecs.resultat_partie rp USING(id_resultat)
+  JOIN echecs.tournoi t USING(id_tournoi)
+ WHERE t.nom = 'Blitz à Rennes';
+
+
+-- Performances
 SELECT CONCAT_WS(' ', LOWER(j1.code_titre), j1.nom, j1.prenom) AS blancs,
        j1.elo,
        rp.resultat,
@@ -166,12 +197,178 @@ SELECT CONCAT_WS(' ', LOWER(j1.code_titre), j1.nom, j1.prenom) AS blancs,
   JOIN echecs.joueuse j2 ON (p.id_noir = j2.id_joueuse)
   JOIN echecs.resultat_partie rp USING(id_resultat)
  WHERE (j1.elo > j2.elo + 400 AND rp.id_resultat = 2)
+    OR (j1.elo < j2.elo - 400 AND rp.id_resultat = 1);
+
+-- Performances classées par différence de elo
+SELECT CONCAT_WS(' ', LOWER(j1.code_titre), j1.nom, j1.prenom) AS blancs,
+       j1.elo,
+       rp.resultat,
+       j2.elo,
+       CONCAT_WS(' ', LOWER(j2.code_titre), j2.nom, j2.prenom) AS noirs,
+       ABS(j1.elo - j2.elo) AS difference_elo
+  FROM echecs.joueuse j1
+  JOIN echecs.partie p ON (p.id_blanc = j1.id_joueuse)
+  JOIN echecs.joueuse j2 ON (p.id_noir = j2.id_joueuse)
+  JOIN echecs.resultat_partie rp USING(id_resultat)
+ WHERE (j1.elo > j2.elo + 400 AND rp.id_resultat = 2)
     OR (j1.elo < j2.elo - 400 AND rp.id_resultat = 1)
-
+ ORDER BY difference_elo DESC;
 
 ----------------------------------------------------------------------
--- Créer la table participante
+-- Gagnantes de tournoi
 ----------------------------------------------------------------------
+
+SELECT DISTINCT t.*
+  FROM echecs.partie p
+  JOIN echecs.joueuse j ON (p.id_blanc = j.id_joueuse OR p.id_noir = j.id_joueuse)
+  JOIN echecs.tournoi t USING(id_tournoi)
+ WHERE j.nom = 'HOU';
+
+-- Création de la vue participante
+CREATE OR REPLACE VIEW echecs.participante AS 
+SELECT DISTINCT p.id_tournoi,
+       p.id_blanc AS id_joueuse
+  FROM echecs.partie p
+ WHERE p.id_tournoi IS NOT NULL
+UNION
+SELECT DISTINCT p.id_tournoi,
+       p.id_noir AS id_joueuse
+  FROM echecs.partie p
+ WHERE p.id_tournoi IS NOT NULL;
+
+SELECT COUNT(1)
+  FROM echecs.participante;
+
+-- Score des blancs par partie
+SELECT p.id_partie,
+       p.id_blanc AS id_joueuse,
+       CASE
+           WHEN rp.resultat IN ('1 - 0', '1 - F') THEN 1 
+           WHEN rp.resultat = 'X - X'             THEN 0.5
+           ELSE 0           
+       END AS score
+  FROM echecs.partie p
+  JOIN echecs.resultat_partie rp USING(id_resultat);
+
+-- Score des blancs par partie
+SELECT p.id_partie,
+       p.id_tournoi,
+       p.id_blanc AS id_joueuse,
+       CASE
+           WHEN rp.resultat IN ('1 - 0', '1 - F') THEN 1 
+           WHEN rp.resultat = 'X - X'             THEN 0.5
+           ELSE 0           
+       END AS score,
+       j.elo AS elo_adversaire
+  FROM echecs.partie p
+  JOIN echecs.resultat_partie rp USING(id_resultat)
+  JOIN echecs.joueuse j ON j.id_joueuse = p.id_noir;
+
+-- Vue partie_score
+CREATE VIEW echecs.partie_score
+AS
+SELECT p.id_partie,
+       p.id_tournoi,
+       p.id_blanc AS id_joueuse,
+       CASE
+           WHEN rp.resultat IN ('1 - 0', '1 - F') THEN 1 
+           WHEN rp.resultat = 'X - X'             THEN 0.5
+           ELSE 0           
+       END AS score,
+       j.elo AS elo_adversaire
+  FROM echecs.partie p
+  JOIN echecs.resultat_partie rp USING(id_resultat)
+  JOIN echecs.joueuse j ON j.id_joueuse = p.id_noir
+UNION
+SELECT p.id_partie,
+       p.id_tournoi,
+       p.id_noir AS id_joueuse,
+       CASE
+           WHEN rp.resultat IN ('0 - 1', 'F - 1') THEN 1 
+           WHEN rp.resultat = 'X - X'             THEN 0.5
+           ELSE 0           
+       END AS score,
+       j.elo AS elo_adversaire
+  FROM echecs.partie p
+  JOIN echecs.resultat_partie rp USING(id_resultat)
+  JOIN echecs.joueuse j ON j.id_joueuse = p.id_blanc;
+
+
+-- Vérifications
+SELECT DISTINCT SUM(score)
+  FROM echecs.partie_score
+ GROUP BY id_partie;
+
+SELECT id_partie, SUM(score)
+  FROM echecs.partie_score
+ GROUP BY id_partie
+HAVING SUM(score) <> 1;
+
+-- Classement Guingamp
+SELECT CONCAT_WS(' ', LOWER(j.code_titre), j.nom, j.prenom) AS joueuse,
+       j.elo,
+       SUM(score) AS points,
+       ROUND(AVG(ps.elo_adversaire)) AS moyenne_elo_adversaire
+  FROM echecs.joueuse j
+  JOIN echecs.partie_score ps USING(id_joueuse)
+  JOIN echecs.tournoi t USING(id_tournoi)
+ WHERE t.nom = 'Open de Guingamp'
+ GROUP BY j.id_joueuse,
+          joueuse,
+          j.elo
+ ORDER BY points DESC,
+          moyenne_elo_adversaire DESC;
+
+
+
+----
+
+
+
+
+
+SELECT p.*,
+       rp.*
+  FROM echecs.participante pa
+  JOIN echecs.joueuse j USING(id_joueuse)
+  JOIN echecs.partie p 
+    ON p.id_tournoi = pa.id_tournoi 
+   AND (j.id_joueuse = p.id_blanc OR j.id_joueuse = p.id_noir)
+  JOIN echecs.resultat_partie rp USING(id_resultat)
+ WHERE p.id_tournoi = 1
+  
+
+CREATE OR REPLACE VIEW echecs.partie_score
+AS
+SELECT p.id_partie,
+       p.id_blanc,
+       p.id_noir,
+       rp.score_blanc,
+       rp.score_noir
+  FROM echecs.partie p 
+  JOIN echecs.resultat_partie rp USING(id_resultat)
+
+SELECT id_partie,
+       UNNEST(ARRAY[id_blanc, id_noir]) AS id_joueuse,
+       UNNEST(ARRAY[score_blanc, score_noir]) AS score
+  FROM echecs.partie_score p
+
+
+WITH a AS( 
+SELECT id_partie,
+       UNNEST(ARRAY[id_blanc, id_noir]) AS id_joueuse,
+       UNNEST(ARRAY[score_blanc, score_noir]) AS score
+  FROM echecs.partie_score p)
+SELECT id_partie, sum(score)
+  from a
+group by 1
+having sum(score) <> 1
+
+
+
+
+
+
 
 
 
