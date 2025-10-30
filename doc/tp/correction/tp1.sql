48d6dd18f8b86c996a3037bcd7f721eb3b1756ca
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2025-10-30 17:33:00 +0000

    Built site for gh-pages

--- doc/tp/correction/tp1.sql
+++ doc/tp/correction/tp1.sql
@@ -1,78 +1,92 @@
-CREATE OR REPLACE VIEW prenom AS
-FROM 'https://static.data.gouv.fr/resources/base-prenoms-insee-format-parquet/20231121-161435/prenoms-nat2022.parquet'
+CREATE OR REPLACE TABLE prenoms AS
+FROM 'https://www.insee.fr/fr/statistiques/fichier/8595130/prenoms-2024.parquet';
 
+SET force_download=true;
 
--------------------------------------------------------------------------------
--- 1ere requêtes
--------------------------------------------------------------------------------
-
--- tous les prénoms
 SELECT *
-  FROM prenom
- WHERE preusuel <> '_PRENOMS_RARES';
+  FROM prenoms
+ WHERE niveau_geographique = 'FRANCE'
+   AND prenom = 'GINETTE';
 
-DESCRIBE FROM prenom;
 
--- Ordonnés par année de naissance DESC
-SELECT DISTINCT annais
-  FROM prenom
- WHERE preusuel <> '_PRENOMS_RARES'
- ORDER BY annais DESC;
+CREATE OR REPLACE TABLE prenoms_fr AS
+SELECT sexe,
+       prenom,
+       periode,
+       valeur
+  FROM prenoms
+ WHERE niveau_geographique = 'FRANCE';
 
--- Sans les données manquantes
-SELECT *
-  FROM prenom
- WHERE preusuel <> '_PRENOMS_RARES'
-   AND annais <> 'XXXX';
+  
+SELECT COUNT(1)
+  FROM prenoms_fr;
 
 -------------------------------------------------------------------------------
 -- Année 2022
 -------------------------------------------------------------------------------
 
 SELECT *
-  FROM prenom
- WHERE preusuel <> '_PRENOMS_RARES'
-   AND annais <> 'XXXX'
-   AND annais = '2022';
+  FROM prenoms_fr
+ WHERE periode = '2022'
+ ORDER BY valeur DESC;
 
--- Donnés plus de 2000 fois classé par sexe et nombre
+-- Colonnes
+SELECT sexe,
+       prenom,
+       valeur
+  FROM niveau_geographique = 'FRANCE'
+   AND periode = '2022'
+   AND valeur >= 2000
+
+-- Donnés 2000 fois ou plus
 SELECT *
-  FROM prenom
- WHERE preusuel <> '_PRENOMS_RARES'
-   AND annais <> 'XXXX'
-   AND annais = '2022'
-   AND nombre > 2000
+  FROM prenoms_fr
+ WHERE periode = '2022'
+   AND valeur >= 2000
+
+-- Classés par sexe et nombre
+SELECT sexe,
+       prenom,
+       valeur
+  FROM prenoms_fr
+ WHERE periode = '2022'
+   AND valeur >= 2000
  ORDER BY sexe,
-          nombre DESC;
+          valeur DESC
 
--- Listez les prénoms féminins commençant par la lettre Q
-SELECT *
-  FROM prenom
- WHERE preusuel <> '_PRENOMS_RARES'
-   AND annais <> 'XXXX'
-   AND annais = '2022'
-   AND sexe = 2
-   AND preusuel LIKE 'Q%';
+-- Prénoms féminins commençant par la lettre Q
+SELECT sexe,
+       prenom,
+       valeur
+  FROM prenoms_fr
+ WHERE periode = '2022'
+   AND sexe = '2'
+   AND prenom LIKE 'Q%'
 
--- Compter le nombre de prénoms commençant par chaque lettre
-SELECT SUBSTRING(preusuel, 1, 1) AS lettre,
-       COUNT(1)
-  FROM prenom
- WHERE preusuel <> '_PRENOMS_RARES'
-   AND annais <> 'XXXX'
-   AND annais = '2022'
- GROUP BY lettre
+-- Prénoms contenant un X et pas de A
+SELECT sexe,
+       prenom,
+       valeur
+  FROM prenoms_fr
+ WHERE periode = '2022'
+   AND prenom LIKE '%X%'
+   AND prenom NOT LIKE '%A%'
+
+-- Nombre de prénoms commençant par chaque lettre
+SELECT SUBSTRING(prenom, 1, 1) AS premiere_lettre,
+       COUNT(1) AS nombre
+  FROM prenoms_fr
+ WHERE periode = '2022'
+ GROUP BY premiere_lettre
  ORDER BY 1;
 
--- Bonus : différenciez filles et gaçons
-SELECT SUBSTRING(preusuel, 1, 1) AS lettre, --preusuel[:1],
+-- Nombre de prénoms par sexe commençant par chaque lettre
+SELECT SUBSTRING(prenom, 1, 1) AS premiere_lettre,
        COUNT(1) FILTER (WHERE sexe = 1) AS garcon,
        COUNT(1) FILTER (WHERE sexe = 2) AS fille
-  FROM prenom
- WHERE preusuel <> '_PRENOMS_RARES'
-   AND annais <> 'XXXX'
-   AND annais = '2022'
- GROUP BY lettre
+  FROM prenoms_fr
+ WHERE periode = '2022'
+ GROUP BY premiere_lettre
  ORDER BY 1;
 
 
@@ -80,122 +94,143 @@ SELECT SUBSTRING(preusuel, 1, 1) AS lettre, --preusuel[:1],
 -- Statistiques descriptives
 -------------------------------------------------------------------------------
 
+SUMMARIZE prenoms;
+
 -- Affichez pour l'année 2003, les prénoms et leurs nombres de caractères
-SELECT preusuel,
-       LENGTH(preusuel)
-  FROM prenom
- WHERE preusuel <> '_PRENOMS_RARES'
-   AND annais = '2003';
+SELECT prenom,
+       LENGTH(prenom)
+  FROM prenoms_fr
+ WHERE periode = '2003';
 
 
 -- Nombres de caractères minimum, maximum et moyen parmi les prénoms de 2003
-SELECT MIN(LENGTH(preusuel)),
-       MAX(LENGTH(preusuel)),
-       AVG(LENGTH(preusuel))
-  FROM prenom
- WHERE preusuel <> '_PRENOMS_RARES'
-   AND annais = '2003';
+SELECT MIN(LENGTH(prenom)),
+       MAX(LENGTH(prenom)),
+       AVG(LENGTH(prenom))
+  FROM prenoms_fr
+ WHERE periode = '2003';
 
 
--- Listez les 3 prénoms de 2003 ayant le nombre de caractères maximum
+-- Listez les 3 prénoms de 2003 ayant le valeur de caractères maximum
 -- step1 : taille max en 2003
-SELECT MAX(LENGTH(preusuel))
-  FROM prenom
- WHERE annais = '2003';
+SELECT MAX(LENGTH(prenom))
+  FROM prenoms_fr
+ WHERE periode = '2003';
 
 -- step2 : utiliser ce résultat dans une sous-requête
-SELECT preusuel
-  FROM prenom
- WHERE annais = '2003'
-   AND LENGTH(preusuel) = (SELECT MAX(LENGTH(preusuel)) 
-                             FROM prenom 
-                            WHERE annais = '2003');
-
+SELECT prenom
+  FROM prenoms_fr
+ WHERE periode = '2003'
+   AND LENGTH(prenom) = (SELECT MAX(LENGTH(prenom)) 
+                             FROM prenoms_fr 
+                            WHERE periode = '2003');
 
 -- afficher pour chaque année entre 2015 et 2022 le ou les prénoms avec le plus de caractères
 -- step1 : taille max par année
-SELECT p2.annais,
-       MAX(LENGTH(p2.preusuel))
-  FROM prenom p2
-  WHERE p2.annais BETWEEN '2015' AND '2022'
-  GROUP BY p2.annais
+SELECT p2.periode,
+       MAX(LENGTH(p2.prenom))
+  FROM prenoms_fr p2
+ WHERE p2.periode BETWEEN '2015' AND '2022'
+  GROUP BY p2.periode
   ORDER BY 1;
 
--- step2 : utiliser la requête ci-dessus comme sous requête et faire le lien avec le champ annais
-SELECT p1.annais,
-       p1.preusuel,
-       LENGTH(p1.preusuel)
-  FROM prenom p1
- WHERE p1.preusuel <> '_PRENOMS_RARES'
-   AND p1.annais BETWEEN '2015' AND '2022'
-   AND LENGTH(preusuel) = (SELECT MAX(LENGTH(p2.preusuel)) 
-                             FROM prenom p2
-                            WHERE p2.preusuel <> '_PRENOMS_RARES'
-                              AND p1.annais = p2.annais)
- ORDER BY annais;
+-- step2 : utiliser la requête ci-dessus comme sous requête et faire le lien avec le champ periode
+SELECT p1.periode,
+       p1.prenom,
+       LENGTH(p1.prenom)
+  FROM prenoms_fr p1
+ WHERE p1.periode BETWEEN '2015' AND '2022'
+   AND LENGTH(prenom) = (SELECT MAX(LENGTH(p2.prenom)) 
+                           FROM prenoms_fr p2
+                          WHERE p1.periode = p2.periode)
+ ORDER BY periode;
 
+-- Affichez pour chaque année la taille moyenne des prénoms
+SELECT periode,
+       ROUND(AVG(LENGTH(prenom)),1)
+  FROM prenoms_fr
+ GROUP BY periode
+ ORDER BY periode;
 
--- Affichez pour chaque année la taille moyenne des prénom
-SELECT annais,
-       ROUND(AVG(LENGTH(preusuel)),1)
-  FROM prenom
- WHERE preusuel <> '_PRENOMS_RARES'
-   AND annais <> 'XXXX'
- GROUP BY annais
- ORDER BY annais;
 
 -------------------------------------------------------------------------------
 -- Jérôme
 -------------------------------------------------------------------------------
 
--- années où le prénom JÉRÔME a été donné
-SELECT DISTINCT annais
-  FROM prenom
- WHERE annais <> 'XXXX'
-   AND preusuel = 'JÉRÔME'
- ORDER BY annais DESC;
+-- Années où le prénom JÉRÔME a été donné
+SELECT DISTINCT periode
+  FROM prenoms_fr
+ WHERE prenom = 'JÉRÔME'
+ ORDER BY periode DESC;
 
 -- JEROME avec d'autres accents / sans accent
 SELECT *
-  FROM prenom
- WHERE annais <> 'XXXX'
-   AND strip_accents(preusuel) = 'JEROME'
- ORDER BY annais DESC;
+  FROM prenoms_fr
+ WHERE strip_accents(prenom) = 'JEROME'
+ ORDER BY periode DESC;
 
--- nombre de fois où le prénom JEROME a été donné, quelle que soit l'accentuation
-SELECT strip_accents(preusuel),
-       annais,
-       SUM(nombre)
-  FROM prenom
- WHERE annais <> 'XXXX'
-   AND strip_accents(preusuel) = 'JEROME'
- GROUP BY strip_accents(preusuel),
-          annais;
- ORDER BY annais DESC;
+-- valeur de fois où le prénom JEROME a été donné, quelle que soit l'accentuation
+SELECT strip_accents(prenom),
+       periode,
+       SUM(valeur)
+  FROM prenoms_fr
+ WHERE strip_accents(prenom) = 'JEROME'
+ GROUP BY strip_accents(prenom),
+          periode
+ ORDER BY periode DESC;
+
+-------------------------------------------------------------------------------
+-- Vérifions le classement
+-------------------------------------------------------------------------------
+
+-- Top 10 prénoms féminins 2023
+SELECT *
+  FROM prenoms_fr
+ WHERE sexe = 2
+   AND periode = '2023'
+ ORDER BY valeur DESC
+ LIMIT 10;
+
+-- 11e au classement
+SELECT *
+  FROM prenoms_fr
+ WHERE sexe = 2
+   AND periode = '2023'
+ ORDER BY valeur DESC
+OFFSET 10
+ LIMIT 1;
+
+-- Classement sans accents
+SELECT STRIP_ACCENTS(prenom),
+       SUM(valeur) AS nb
+  FROM prenoms_fr
+ WHERE sexe = 2
+   AND periode = '2023'
+ GROUP BY STRIP_ACCENTS(prenom)
+ ORDER BY nb DESC
+ LIMIT 10;
 
 -------------------------------------------------------------------------------
 -- Suivi temporel
 -------------------------------------------------------------------------------
 
--- Nombre de naissances par année
-SELECT annais,
-       SUM(nombre) AS nb_naissances,
+-- valeur de naissances par année
+SELECT periode,
+       SUM(valeur) AS nb_naissances,
        COUNT(1) AS nb_prenoms
-  FROM prenom
- WHERE annais <> 'XXXX'
-   AND annais >= '2000'
- GROUP BY annais
- ORDER BY annais DESC;
+  FROM prenoms_fr
+ GROUP BY periode
+ ORDER BY periode DESC;
 
--- Nombre de filles et garçons
-SELECT annais,
-       SUM(nombre) FILTER (WHERE sexe = 1) AS nb_garcons,
-       SUM(nombre) FILTER (WHERE sexe = 2) AS nb_filles
-  FROM prenom
- WHERE annais <> 'XXXX'
-   AND annais >= '2000'
- GROUP BY annais
- ORDER BY annais DESC;
+-- valeur de filles et garçons
+SELECT periode,
+       SUM(valeur) FILTER (WHERE sexe = 1) AS nb_garcons,
+       SUM(valeur) FILTER (WHERE sexe = 2) AS nb_filles
+  FROM prenoms_fr
+ GROUP BY periode
+ ORDER BY periode DESC;
+
+
 
 
 -------------------------------------------------------------------------------
@@ -203,39 +238,39 @@ SELECT annais,
 -------------------------------------------------------------------------------
 
 -- prénoms composés entre les années 2000 et 2009 incluses
-SELECT preusuel
-  FROM prenom
- WHERE preusuel LIKE '%-%'
-   AND annais BETWEEN '2000' AND '2009';
+SELECT DISTINCT prenom
+  FROM prenoms_fr
+ WHERE prenom LIKE '%-%'
+   AND periode BETWEEN '2000' AND '2009';
 
--- affichez également le nombre de fois où ils ont été donnés, trié en décroissant
-SELECT preusuel,
-       SUM(nombre) AS nb_donnes
-  FROM prenom
- WHERE preusuel LIKE '%-%'
-   AND annais BETWEEN '2000' AND '2009'
- GROUP BY preusuel
+-- affichez également le valeur de fois où ils ont été donnés, trié en décroissant
+SELECT prenom,
+       SUM(valeur) AS nb_donnes
+  FROM prenoms_fr
+ WHERE prenom LIKE '%-%'
+   AND periode BETWEEN '2000' AND '2009'
+ GROUP BY prenom
  ORDER BY nb_donnes DESC;
 
 -- Prénoms composés contenant JEAN
-SELECT preusuel,
-       SUM(nombre) AS nb_donnes,
-  FROM prenom
- WHERE preusuel LIKE '%-%'
-   AND preusuel LIKE '%JEAN%'
-   AND annais BETWEEN '2000' AND '2009'
- GROUP BY preusuel
+SELECT prenom,
+       SUM(valeur) AS nb_donnes,
+  FROM prenoms
+ WHERE prenom LIKE '%-%'
+   AND prenom LIKE '%JEAN%'
+   AND periode BETWEEN '2000' AND '2009'
+ GROUP BY prenom
  ORDER BY nb_donnes DESC;
 
 -- Pour retirer les JEANNE...
-SELECT preusuel,
-       SUM(nombre) AS nb_donnes,
-  FROM prenom
- WHERE preusuel LIKE '%-%'
-   AND preusuel LIKE '%JEAN%'
-   AND (split_part(preusuel, '-', 1) = 'JEAN' OR split_part(preusuel, '-', 2) = 'JEAN')
-   AND annais BETWEEN '2000' AND '2009'
- GROUP BY preusuel
+SELECT prenom,
+       SUM(valeur) AS nb_donnes,
+  FROM prenoms_fr
+ WHERE prenom LIKE '%-%'
+   AND prenom LIKE '%JEAN%'
+   AND prenom NOT LIKE '%JEANNE%'
+   AND periode BETWEEN '2000' AND '2009'
+ GROUP BY prenom
  ORDER BY nb_donnes DESC;
 
 
@@ -243,43 +278,37 @@ SELECT preusuel,
 -- Cette année-là
 -------------------------------------------------------------------------------
 
--- Nombre de prénoms distincts en 2016
-SELECT COUNT(DISTINCT preusuel) AS nb_prenoms,
-       COUNT(DISTINCT preusuel) FILTER (WHERE sexe = 2) AS prenoms_filles,
-       COUNT(DISTINCT preusuel) FILTER (WHERE sexe = 1) AS prenoms_garcons,
+-- valeur de prénoms distincts en 2016
+SELECT COUNT(DISTINCT prenom) AS nb_prenoms,
+       COUNT(DISTINCT prenom) FILTER (WHERE sexe = 2) AS prenoms_filles,
+       COUNT(DISTINCT prenom) FILTER (WHERE sexe = 1) AS prenoms_garcons,
        prenoms_filles + prenoms_garcons
-  FROM prenom
- WHERE preusuel <> '_PRENOMS_RARES'
-   AND annais = '1962';
+  FROM prenoms_fr
+ WHERE periode = '1962';
 
 -- Prénoms 1962 donnés à des filles et des garçons
-SELECT preusuel,
-       SUM(nombre) AS total,
-       SUM(nombre) FILTER (WHERE sexe = 2) AS total_filles,
-       SUM(nombre) FILTER (WHERE sexe = 1) AS total_garcons
-  FROM prenom
- WHERE preusuel <> '_PRENOMS_RARES'
-   AND annais <> 'XXXX'
-   AND annais = '1962'
- GROUP BY preusuel
+SELECT prenom,
+       SUM(valeur) AS total,
+       SUM(valeur) FILTER (WHERE sexe = 2) AS total_filles,
+       SUM(valeur) FILTER (WHERE sexe = 1) AS total_garcons
+  FROM prenoms_fr
+ WHERE periode = '1962'
+ GROUP BY prenom
  HAVING COUNT(DISTINCT sexe) = 2  -- Le prénom doit avoir été donné aux deux sexes
 ORDER BY total DESC;
 
 -- Prénoms plus donnés aux filles
-SELECT preusuel,
-       SUM(nombre) AS total,
-       SUM(nombre) FILTER (WHERE sexe = 2) AS total_filles,
-       SUM(nombre) FILTER (WHERE sexe = 1) AS total_garcons,
+SELECT prenom,
+       SUM(valeur) AS total,
+       SUM(valeur) FILTER (WHERE sexe = 2) AS total_filles,
+       SUM(valeur) FILTER (WHERE sexe = 1) AS total_garcons,
        total_filles > total_garcons
-  FROM prenom
- WHERE preusuel <> '_PRENOMS_RARES'
-   AND annais <> 'XXXX'
-   AND annais = '1962'
- GROUP BY preusuel
+  FROM prenoms_fr
+ WHERE periode = '1962'
+ GROUP BY prenom
  HAVING COUNT(DISTINCT sexe) = 2  -- Le prénom doit avoir été donné aux deux sexes
     AND total_filles > total_garcons  -- Plus donné aux filles
 ORDER BY total DESC;
-
 
 
 -------------------------------------------------------------------------------
@@ -290,7 +319,7 @@ CREATE OR REPLACE VIEW individus AS
 FROM 'https://static.data.gouv.fr/resources/recensement-de-la-population-fichiers-detail-individus-localises-au-canton-ou-ville-2020-1/20231023-122841/fd-indcvi-2020.parquet'; 
 
 
--- nombre d'individus
+-- valeur d'individus
 SELECT COUNT(1)
   FROM individus;
 
@@ -315,14 +344,14 @@ SELECT *
   FROM variables_individus
  WHERE lib_var ILIKE '%partem%';
 
--- nombre d'habitants par départements
+-- valeur d'habitants par départements
 SELECT dept,
        SUM(ipondi)::INT AS nb_hab
   FROM individus
  GROUP BY dept
  ORDER BY 1;
 
--- nombre d'habitants par départements par sexe entre 25 et 29 ans
+-- valeur d'habitants par départements par sexe entre 25 et 29 ans
 SELECT dept,
        SUM(ipondi) FILTER(WHERE sexe = 1)::INT AS hommes,
        SUM(ipondi) FILTER(WHERE sexe = 2)::INT AS femmes
